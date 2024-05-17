5aa924260f0e4b550ec9dc2abed0ae7205b14293
Lucas Santana <santanafx@hotmail.com>
2024-05-17 01:11:34 -0300

    feat: added home route and store configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { Provider } from 'react-redux'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import PageNotFound from './pages/PageNotFound/PageNotFound'
+import { store } from './store/store'
+
 function App() {
+
+  const router = createBrowserRouter([{
+    path: "/",
+    element: <Home />,
+    errorElement: <PageNotFound />
+  }])
+
   return (
-    <div>teste</div>
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
   )
 }
 
