f3094bcb2b96dc19676c9416c92e877d51005361
Lucas Santana <santanafx@hotmail.com>
2024-05-19 19:27:50 -0300

    feat: added condition to render information

--- src/components/TickerInfo/TickerInfo.tsx
+++ src/components/TickerInfo/TickerInfo.tsx
@@ -13,19 +13,19 @@ export default function TickerInfo() {
       <div className={style.tickerInfoContainer__information}>
         <h2>{symbolFromStore}</h2>
         <p>Last Price:</p>
-        <span>{selectedTickerInfo.lastPrice}</span>
+        <span>{selectedTickerInfo.lastPrice !== "" ? selectedTickerInfo.lastPrice : "-"}</span>
         <p>Price Change:</p>
-        <span>{selectedTickerInfo.priceChange}</span>
+        <span>{selectedTickerInfo.priceChange !== "" ? selectedTickerInfo.priceChange : "-"}</span>
         <p>Price Change Percent:</p>
-        <span>{selectedTickerInfo.priceChangePercent}%</span>
+        <span>{selectedTickerInfo.priceChangePercent !== "" ? `${selectedTickerInfo.priceChangePercent}%` : "-"}</span>
         <p>Highest Price:</p>
-        <span>{selectedTickerInfo.highPrice}</span>
+        <span>{selectedTickerInfo.highPrice !== "" ? selectedTickerInfo.highPrice : "-"}</span>
         <p>Lowest Price:</p>
-        <span>{selectedTickerInfo.lowPrice}</span>
+        <span>{selectedTickerInfo.lowPrice !== "" ? selectedTickerInfo.lowPrice : "-"}</span>
         <p>Volume:</p>
-        <span>{selectedTickerInfo.volume}</span>
+        <span>{selectedTickerInfo.volume !== "" ? selectedTickerInfo.volume : "-"}</span>
         <p>Weighted Average Price:</p>
-        <span>{selectedTickerInfo.weightedAvgPrice}</span>
+        <span>{selectedTickerInfo.weightedAvgPrice !== "" ? selectedTickerInfo.weightedAvgPrice : "-"}</span>
       </div>
     </div>
 
