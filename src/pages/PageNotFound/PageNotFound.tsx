72c2c311fd26a3d5fa34228a9734e66fd9642843
Lucas Santana <santanafx@hotmail.com>
2024-05-19 15:31:05 -0300

    refactor: changed css and functionality to PageNotFound component

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,7 +1,18 @@
-
+import { useNavigate } from "react-router-dom"
+import style from "./PageNotFound.module.css"
 
 export default function PageNotFound() {
+  const navigate = useNavigate()
+
+  const handleGoToHomePage = () => {
+    navigate("/")
+  }
+
   return (
-    <div>PageNotFound</div>
+    <div className={style.pageNotFoundContainer}>
+      <h1>PAGE NOT FOUND</h1>
+      <p>We cant't find the page you're looking for. Please, head back to home.</p>
+      <button className={style.pageNotFoundContainer__button} onClick={() => handleGoToHomePage()}>Home page</button>
+    </div>
   )
 }
